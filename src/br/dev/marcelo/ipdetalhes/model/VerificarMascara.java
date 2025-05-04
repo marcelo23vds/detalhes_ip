8e10526d239b9e29c47c81e7fa8479786871fb5d
Marcelo Vieira <vieiramarcelo.vds@gmail.com>
2025-05-04 20:28:40 -0300

    adicionada a funcionalidade para retornar a mascara em padrão binario

--- src/br/dev/marcelo/ipdetalhes/model/VerificarMascara.java
+++ src/br/dev/marcelo/ipdetalhes/model/VerificarMascara.java
@@ -23,14 +23,14 @@ public class VerificarMascara {
 		} else {
 			
 //			0xFFFFFFFF é um valor numérico, usado para operações com bits.
-			int mascaraDecimal = 0xFFFFFFFF << (32 - mascara);
+			int mascaraHexadecimal = 0xFFFFFFFF << (32 - mascara);
 			
 //			quebrando em 4 octetos e enviando para direita os bits "ligados" em cada um dos octetos com >>> 
 //			e com & 0xFF limpar os bits restantes a esquerda
-		    int octeto1 = (mascaraDecimal >>> 24) & 0xFF;
-		    int octeto2 = (mascaraDecimal >>> 16) & 0xFF;
-		    int octeto3 = (mascaraDecimal >>> 8) & 0xFF;
-		    int octeto4 = mascaraDecimal & 0xFF;
+		    int octeto1 = (mascaraHexadecimal >>> 24) & 0xFF;
+		    int octeto2 = (mascaraHexadecimal >>> 16) & 0xFF;
+		    int octeto3 = (mascaraHexadecimal >>> 8) & 0xFF;
+		    int octeto4 = mascaraHexadecimal & 0xFF;
 		    
 //		    imprimindo apenas os bits ligados em cada octeto, deixando assim na ordem do padrão de uma máscara 
 		    return "Máscara em Decimal: " + octeto1 + "." + octeto2 + "." + octeto3 + "." + octeto4;
@@ -40,7 +40,26 @@ public class VerificarMascara {
 	
 	public String verificarBinario() {
 		
-		return "Teste";
+		if (mascara < 8 || mascara > 32) {
+			
+			return "Máscara inválida!";
+			
+		} else {
+			
+			int mascaraHexadecimal = 0xFFFFFFFF << (32 - mascara);
+			
+//			os tipos Int sao convertidos para String 
+//			e o String.format("%8s", ...) formata para ter 8 caracteres em cada octeto
+//			o >>> envia para direita os bits ligados e o & 0xFF limpa os da esquerda
+//			o replace vai substituir o que estiver vazio, ou seja, onde seriam os bits desligados por 0
+	        String octeto1 = String.format("%8s", Integer.toBinaryString((mascaraHexadecimal >>> 24) & 0xFF)).replace(' ', '0');
+	        String octeto2 = String.format("%8s", Integer.toBinaryString((mascaraHexadecimal >>> 16) & 0xFF)).replace(' ', '0');
+	        String octeto3 = String.format("%8s", Integer.toBinaryString((mascaraHexadecimal >>> 8) & 0xFF)).replace(' ', '0');
+	        String octeto4 = String.format("%8s", Integer.toBinaryString(mascaraHexadecimal & 0xFF)).replace(' ', '0');
+			
+			return "Máscara em Binario: " + octeto1 + "." + octeto2 + "." + octeto3 + "." + octeto4;
+			
+		}
 		
 	}
 	
